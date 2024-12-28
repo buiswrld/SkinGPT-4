6f6967ae606c23b07a96bbbafc40d37e1b683088
Ryan Bui <buiswrld@gmail.com>
2024-12-28 04:44:10 -0800

    add test for output layer

---
+++ class_test.py
@@ -0,0 +1,42 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import pandas as pd
+from skingpt4.models.skin_gpt4 import skingpt4
+from classification_task import ClassificationTask
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+])
+
+params = {
+    "vit_model": "eva_clip_g",
+    "q_former_model": "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/blip2_pretrained_flant5xxl.pth",
+    "img_size": 224,
+    "drop_path_rate": 0,
+    "use_grad_checkpoint": False,
+    "vit_precision": "fp16",
+    "freeze_vit": True,
+    "freeze_qformer": True,
+    "num_query_token": 32,
+    "low_resource": False,
+    "device_8bit": 0,
+    "loss_fn": "CE",
+    "dataset_path": "data/test_csv.csv",
+}
+model = ClassificationTask(params)
+model.eval()
+
+test_images = [
+    "dataset/images/test_image.png",
+    "dataset/images/test_image2.png",
+]
+
+for image_path in test_images:
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image).unsqueeze(0) 
+    with torch.no_grad():
+        logits = model(image)
+        predicted_class = torch.argmax(logits, dim=1).item()
+        print(f"Image: {image_path}, Predicted Class: {predicted_class}")
