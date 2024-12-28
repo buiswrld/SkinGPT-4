f2c388077c8c06a655affcc39a7271eef283ce49
Ryan Bui <buiswrld@gmail.com>
2024-12-28 03:32:13 -0800

    strip llama/alignment layers and add linear layer for 6 outputs

--- skingpt4/models/skin_gpt4.py
+++ skingpt4/models/skin_gpt4.py
@@ -83,43 +83,11 @@ class skingpt4(Blip2Base):
             logging.info("freeze Qformer")
         print('Loading Q-Former Done')
 
-        print('Loading LLM tokenizer')
-        self.llm_tokenizer = LlamaTokenizer.from_pretrained(llm_model, use_fast=False)
-        self.llm_tokenizer.pad_token = self.llm_tokenizer.eos_token
-
-        print('Loading LLM model')
-        if self.low_resource:
-            self.llm_model = LlamaForCausalLM.from_pretrained(
-                llm_model,
-                torch_dtype=torch.float16,
-                load_in_8bit=True,
-                device_map={'': device_8bit}
-            )
-        else:
-            self.llm_model = LlamaForCausalLM.from_pretrained(
-                llm_model,
-                torch_dtype=torch.float16,
-            )
-
-        for name, param in self.llm_model.named_parameters():
-            param.requires_grad = False
-        print('Loading LLM Done')
-
-        self.llama_proj = nn.Linear(
-            self.Qformer.config.hidden_size, self.llm_model.config.hidden_size
+        self.classification_head = nn.Linear(
+            self.Qformer.config.hidden_size, 6
         )
         self.max_txt_len = max_txt_len
         self.end_sym = end_sym
-
-        if prompt_path:
-            with open(prompt_path, 'r') as f:
-                raw_prompts = f.read().splitlines()
-            filted_prompts = [raw_prompt for raw_prompt in raw_prompts if "<ImageHere>" in raw_prompt]
-            self.prompt_list = [prompt_template.format(p) for p in filted_prompts]
-            print('Load {} training prompts'.format(len(self.prompt_list)))
-            print('Prompt Example \n{}'.format(random.choice(self.prompt_list)))
-        else:
-            self.prompt_list = []
 
     def vit_to_cpu(self):
         self.ln_vision.to("cpu")
@@ -149,77 +117,12 @@ class skingpt4(Blip2Base):
             atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(image.device)
         return inputs_llama, atts_llama
 
-    def prompt_wrap(self, img_embeds, atts_img, prompt):
-        if prompt:
-            batch_size = img_embeds.shape[0]
-            p_before, p_after = prompt.split('<ImageHere>')
-            p_before_tokens = self.llm_tokenizer(
-                p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
-            p_after_tokens = self.llm_tokenizer(
-                p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
-            p_before_embeds = self.llm_model.model.embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
-            p_after_embeds = self.llm_model.model.embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
-            wrapped_img_embeds = torch.cat([p_before_embeds, img_embeds, p_after_embeds], dim=1)
-            wrapped_atts_img = atts_img[:, :1].expand(-1, wrapped_img_embeds.shape[1])
-            return wrapped_img_embeds, wrapped_atts_img
-        else:
-            return img_embeds, atts_img
 
     def forward(self, samples):
         image = samples["image"]
         img_embeds, atts_img = self.encode_img(image)
-        if hasattr(samples, 'question_split'):  # VQA dataset
-            print('VQA Batch')
-            vqa_prompt = '###Human: <Img><ImageHere></Img> '
-            img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img, vqa_prompt)
-        elif self.prompt_list:
-            prompt = random.choice(self.prompt_list)
-            img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img, prompt)
-
-        self.llm_tokenizer.padding_side = "right"
-
-        text = [t + self.end_sym for t in samples["text_input"]]
-
-        to_regress_tokens = self.llm_tokenizer(
-            text,
-            return_tensors="pt",
-            padding="longest",
-            truncation=True,
-            max_length=self.max_txt_len,
-            add_special_tokens=False
-        ).to(image.device)
-
-        targets = to_regress_tokens.input_ids.masked_fill(
-            to_regress_tokens.input_ids == self.llm_tokenizer.pad_token_id, -100
-        )
-
-        empty_targets = (
-            torch.ones([atts_img.shape[0], atts_img.shape[1]+1],
-                       dtype=torch.long).to(image.device).fill_(-100)  # plus one for bos
-        )
-        targets = torch.cat([empty_targets, targets], dim=1)
-
-        batch_size = img_embeds.shape[0]
-        bos = torch.ones([batch_size, 1],
-                         dtype=to_regress_tokens.input_ids.dtype,
-                         device=to_regress_tokens.input_ids.device) * self.llm_tokenizer.bos_token_id
-        bos_embeds = self.llm_model.model.embed_tokens(bos)
-        atts_bos = atts_img[:, :1]
-
-        to_regress_embeds = self.llm_model.model.embed_tokens(to_regress_tokens.input_ids)
-        inputs_embeds = torch.cat([bos_embeds, img_embeds, to_regress_embeds], dim=1)
-        attention_mask = torch.cat([atts_bos, atts_img, to_regress_tokens.attention_mask], dim=1)
-
-        with self.maybe_autocast():
-            outputs = self.llm_model(
-                inputs_embeds=inputs_embeds,
-                attention_mask=attention_mask,
-                return_dict=True,
-                labels=targets,
-            )
-        loss = outputs.loss
-
-        return {"loss": loss}
+        logits = self.classification_head(img_embeds)
+        return logits
 
     @classmethod
     def from_config(cls, cfg):
@@ -227,7 +130,6 @@ class skingpt4(Blip2Base):
         q_former_model = cfg.get("q_former_model", "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/blip2_pretrained_flant5xxl.pth")
         img_size = cfg.get("image_size")
         num_query_token = cfg.get("num_query_token")
-        llm_model = cfg.get("llm_model")
 
         drop_path_rate = cfg.get("drop_path_rate", 0)
         use_grad_checkpoint = cfg.get("use_grad_checkpoint", False)
@@ -236,11 +138,6 @@ class skingpt4(Blip2Base):
         freeze_qformer = cfg.get("freeze_qformer", True)
         low_resource = cfg.get("low_resource", False)
         device_8bit = cfg.get("device_8bit", 0)
-
-        prompt_path = cfg.get("prompt_path", "")
-        prompt_template = cfg.get("prompt_template", "")
-        max_txt_len = cfg.get("max_txt_len", 32)
-        end_sym = cfg.get("end_sym", '\n')
 
         model = cls(
             vit_model=vit_model,
@@ -252,19 +149,7 @@ class skingpt4(Blip2Base):
             freeze_vit=freeze_vit,
             freeze_qformer=freeze_qformer,
             num_query_token=num_query_token,
-            llm_model=llm_model,
-            prompt_path=prompt_path,
-            prompt_template=prompt_template,
-            max_txt_len=max_txt_len,
-            end_sym=end_sym,
             low_resource=low_resource,
             device_8bit=device_8bit,
         )
 
-        ckpt_path = cfg.get("ckpt", "")  # load weights of SkinGPT-4-llama2
-        if ckpt_path:
-            print("Load BLIP2-LLM Checkpoint: {}".format(ckpt_path))
-            ckpt = torch.load(ckpt_path, map_location="cpu")
-            msg = model.load_state_dict(ckpt['model'], strict=False)
-
-        return model
