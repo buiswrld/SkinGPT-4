e03cfe7095f5abe6b7cd053f51e4220ed118f1d5
Ryan Bui <buiswrld@gmail.com>
2024-12-29 18:39:27 -0800

    fix import statement and usage of model

--- skingpt4/models/detection/__init__.py
+++ skingpt4/models/detection/__init__.py
@@ -1,7 +1,7 @@
 import argparse
 from util import Args
 from skingpt4.models import load_model
-from skingpt4.models import skingpt4
+from skingpt4.models import skin_gpt4
 
 def get_model(model_args: dict):
     model_args_ = model_args
@@ -10,6 +10,6 @@ def get_model(model_args: dict):
         model_args_ = Args(vars(model_args))
 
     if model_args_.get('task') == "classification":
-        model = skingpt4.from_configs(model_args)
+        model = skin_gpt4.from_configs(model_args)
  
     return model 
