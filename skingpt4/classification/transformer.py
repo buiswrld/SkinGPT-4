09df1156e22f008630390e423a65652ec4622168
Ryan Bui <buiswrld@gmail.com>
2025-01-13 14:59:05 -0800

    separate transforming concerns into different functions

--- skingpt4/classification/transformer.py
+++ skingpt4/classification/transformer.py
@@ -4,18 +4,21 @@ class Transformer:
     def __init__(self):
         self.transforms = [ transforms.Resize((810, 1080)),transforms.ToTensor()]
 
-    def init_train(self, downsample_dim, starting_img_size):
+    def downsample(self, downsample_dim, starting_img_size):
         downsample_dim = downsample_dim
         starting_img_size = starting_img_size
-        downsample_transforms = transforms.Compose([
+        downsample_transforms = [
             transforms.Resize((downsample_dim, downsample_dim)),
             transforms.Resize((starting_img_size, starting_img_size))
-        ])
-    
-        train_transforms = [
+        ]
+
+        self.transforms = downsample_transforms + self.transforms
+
+    def randomize(self):
+        random_transforms = [
             transforms.RandomHorizontalFlip(0.5),
             transforms.RandomVerticalFlip(0.5),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05)
         ]
 
-        self.transforms = [downsample_transforms] + train_transforms + self.transforms
+        self.transforms = random_transforms + self.transforms
