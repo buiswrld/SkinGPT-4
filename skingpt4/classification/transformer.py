37aa43cda9d25b6e4df60fa4e79a2c5838f248c9
Ryan Bui <buiswrld@gmail.com>
2025-01-15 10:50:16 -0800

    add documentation

--- skingpt4/classification/transformer.py
+++ skingpt4/classification/transformer.py
@@ -5,6 +5,13 @@ class Transformer:
         self.transforms = [ transforms.Resize((1080, 810))]
 
     def downsample(self, downsample_factor: float) -> None:
+        """
+        Add transformations that downsample the image by a certain factor and enforce 1080x810 size
+
+        Args:
+            downsample_factor (float): The factor by which to downsample the image
+
+        """
         h = downsample_factor * 1080
         w = downsample_factor * 810
         downsample_transforms = [
@@ -15,6 +22,12 @@ class Transformer:
         self.transforms = downsample_transforms + self.transforms
 
     def randomize_img(self, degree = 1) -> None:
+        """
+        Add transformations that introduce image variation (flips, color jitter)
+
+        Args:
+            degree (int): The degree of variation to introduce (affects magnitude of color jittering only)
+        """
         random_transforms = [
             transforms.RandomHorizontalFlip(0.5),
             transforms.RandomVerticalFlip(0.5),
@@ -24,7 +37,16 @@ class Transformer:
         self.transforms = self.transforms + random_transforms
 
     def to_tensor(self) -> None:
+        """
+        Add transformation that converts the image to a tensor
+        """
         self.transforms = self.transforms + [transforms.ToTensor()]
 
     def transforms(self) -> transforms.Compose:
+        """
+        Return all transformations from this class instance
+
+        Returns:
+            transforms.Compose: The composed transformations
+        """
         return transforms.Compose(self.transforms)
