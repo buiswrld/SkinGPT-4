9afc1922f15d5135c8f00c66e2adb2cf33e8fe83
Ryan Bui <buiswrld@gmail.com>
2025-01-21 23:08:45 -0800

    improve description for downsample parameter

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def train(
         num_classes=6, #TODO ~ Customize
         oversample=False,
         oversample_col = 'label',#TODO ~ Customize (eligible: 'label', 'fitz')
-        downsample_factor=1.0, #TODO ~ Customize (determines the percentage of image quality)
+        downsample_factor=1.0, #TODO ~ Customize (determines the percentage of image quality, 1.0 indicates no downsampling, 0.5 indicates 50% downsampling)
         oversample_factor=1.0, #TODO ~ Customize (determines the oversampling factor, magnitude 1 indicates a balanced set, >1 increases minority class sapmples)
         data_regime=1.0, #TODO ~ Customize (determines the percentage of training data to use)
         ## model params
